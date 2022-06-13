2ef5817e169b56fc710065801fef26356b8df95b
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-06-13 17:36:48 +0200

    color change

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ document.getElementById("userName").innerHTML = userName;
 
 
 
-
-
 let lastName = document.getElementById("lastName").innerHTML;
 console.log(lastName)
 
@@ -27,3 +25,33 @@ userFavoriteColor = userFavoriteColor + prompt("Inserisci il tuo colore preferit
 console.log(userFavoriteColor);
 
 document.getElementById("userFavoriteColor").innerHTML = userFavoriteColor;
+
+
+
+/* test */
+
+let r, g, b, colorRandom;
+
+r = Math.round(Math.random() * 256);
+g = Math.round(Math.random() * 256);
+b = Math.round(Math.random() * 256);
+
+colorRandom = "rgb(" + r + ", " + g + ", " + b + ")";
+console.log(colorRandom);
+
+document.getElementById("title").style.color = colorRandom;
+
+
+
+
+document.getElementById("title").onclick = function () {
+
+    r = Math.round(Math.random() * 256);
+    g = Math.round(Math.random() * 256);
+    b = Math.round(Math.random() * 256);
+
+    colorRandom = "rgb(" + r + ", " + g + ", " + b + ")";
+    console.log(colorRandom);
+
+    document.getElementById("title").style.backgroundColor = colorRandom;
+}
